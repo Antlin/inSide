9972c3d7f905dcf9410bc1d1c77854d8c6701f72
Antlin <1101198481@qq.com>
2017-03-29 22:38:10 +0800

    重新上传项目

---
+++ inSide/inSide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  inSide
+//
+//  Created by 林志伟 on 2017/3/29.
+//  Copyright © 2017年 com.antlin.inside. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ inSide/inSide/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  inSide
+//
+//  Created by 林志伟 on 2017/3/29.
+//  Copyright © 2017年 com.antlin.inside. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
